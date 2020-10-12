a49be63d60e233d783706189854b2bf3e6711c2b
Willian Mz <willianmazzorana@hotmail.com>
2020-10-12 14:59:20 -0300

    Implementado a base da classe CategoryForm

--- src/app/pages/categories/categoria-form/categoria-form.component.ts
+++ src/app/pages/categories/categoria-form/categoria-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterContentChecked } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validator, Validators} from '@angular/forms';
-import { ActivatedRoute, Router, ActivationEnd} from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { ActivatedRoute, Router} from '@angular/router';
 
 import { Category} from '../shared/category.model';
 import { CategoryService} from '../shared/category.service';
@@ -17,10 +17,13 @@ import { Route } from '@angular/compiler/src/core';
 })
 export class CategoriaFormComponent implements OnInit, AfterContentChecked {
 
+  //ação atual: new/edit
   currentAction: string;
   categoryForm: FormGroup;
   pageTitle: string;
+  //erros que ocorrem no servidor
   serverErrorMessages: string[] = null;
+  //desabilita botao de enviar e habilta quando o servidor retornar
   submittingForm: boolean = false;
   category: Category = new Category();
 
@@ -38,19 +41,22 @@ export class CategoriaFormComponent implements OnInit, AfterContentChecked {
   }
 
   ngAfterContentChecked(){
+    //seta o titulo da pagina
     this.setPageTitle();
   }
 
   //METODOS PRIVADOS
 
   private setCurrentAction(){
-    if (this.route.snapshot.url[0].path == "new")
-      this.currentAction = "new"
+    //verifica a ação que esta sendo executada. Novo/Editar
+    if (this.route.snapshot.url[0].path == 'new')
+      this.currentAction = 'new'
     else
-      this.currentAction = "edit"
+      this.currentAction = 'edit'
   }
 
   private buildCategoryForm(){
+    //constroi o form da categoria
     this.categoryForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
@@ -59,13 +65,16 @@ export class CategoriaFormComponent implements OnInit, AfterContentChecked {
   }
 
   private loadCategory(){
-    if(this.currentAction == "edit")
+    //caso a acao atual seja de edição
+    if(this.currentAction == 'edit')
       this.route.paramMap.pipe(
-        switchMap(params => this.categoryService.getById(+params.get("id")))
-      )
-      .subscribe(
-        (category) => {
+        switchMap(params => 
+          this.categoryService.getById(+params.get('id')))//+ transforma pra numero
+      ).subscribe(
+        (category) => 
+        {
           this.category = category;
+          //preenche os campos do formulario com dados retornados do servidor
           this.categoryForm.patchValue(category);
         },
         (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
@@ -73,10 +82,10 @@ export class CategoriaFormComponent implements OnInit, AfterContentChecked {
   }
 
   private setPageTitle(){
-    if(this.currentAction == "new")
-      this.pageTitle = "Cadastro de nova categoria"
+    if(this.currentAction == 'new')
+      this.pageTitle = 'Cadastro de nova categoria'
     else{
-      const categoryName = this.category.name || "";
+      const categoryName = this.category.name || ""
       this.pageTitle = 'Editando categoria: ' + categoryName;
     }      
   }
